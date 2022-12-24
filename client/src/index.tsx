1800af66c2d623a5f53bdf0be24d4f6ad9bb1ae9
Luca <61409445+melusc@users.noreply.github.com>
2022-12-24 18:33:53 +0100

    Use workspaces

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client.js';
 
-import {App} from './components/App';
+import {App} from './components/app';
 
 const root = createRoot(document.querySelector('#root')!);
 root.render(<App />);
